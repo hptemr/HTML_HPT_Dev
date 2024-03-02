b123b973730a33aacb6a03ae58e433ff81671f33
thoratprashant <prashant.thorat@arkenea.com>
2024-03-03 02:00:49 +0530

    Added delete account modal

--- src/app/component/system-admin/user-managment/practice-admin-profile/practice-admin-profile.component.ts
+++ src/app/component/system-admin/user-managment/practice-admin-profile/practice-admin-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AlertComponent } from 'src/app/shared/comman/alert/alert.component';
 
 @Component({
   selector: 'app-practice-admin-profile', 
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './practice-admin-profile.component.scss'
 })
 export class PracticeAdminProfileComponent {
+  constructor(private router: Router, public dialog: MatDialog) { }
 
+  deleteAccount() {
+    const dialogRef = this.dialog.open(AlertComponent,{
+      panelClass: 'custom-alert-container',
+    });
+  }
 }
