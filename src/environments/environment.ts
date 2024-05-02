34710e20a55efd0853941c670dc0adb7479cab35
Ashish <shahnawaz@arkenea.com>
2024-05-02 12:42:52 +0530

    environment file changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',
+    // apiUrl: 'http://localhost:3000/api', // Local URL
+    apiUrl: 'https://ec2-34-239-203-1.compute-1.amazonaws.com/api',  // Server URL (Use at the time of build)
 };
