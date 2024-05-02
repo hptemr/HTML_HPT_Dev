32b35494741f9b04055546bc424c12644b026e6a
Ashish <shahnawaz@arkenea.com>
2024-05-02 12:50:43 +0530

    1)Fixed issue related system admin users 2)Merging Pankaj, Manoj and My branch code in main branch.

--- config/constants.js
+++ config/constants.js
@@ -13,8 +13,8 @@ var constants = {
 	  secret: "",
     fromEmail: "pankajk@arkenea.com",
   },
-  clientUrl: process.env.clientUrl || 'http://ec2-34-239-203-1.compute-1.amazonaws.com:8080',
-  mailServerUrl : process.env.mailServerUri || 'http://ec2-34-239-203-1.compute-1.amazonaws.com:8080',
+  clientUrl: process.env.clientUrl || 'https://ec2-34-239-203-1.compute-1.amazonaws.com',
+  mailServerUrl : process.env.mailServerUri || 'https://ec2-34-239-203-1.compute-1.amazonaws.com',
 }
 
 module.exports = constants
