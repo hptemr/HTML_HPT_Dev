9f268bf3e7c9834e4ee85022832f684f7edd3079
thoratprashant <prashant.thorat@arkenea.com>
2024-05-22 17:32:36 +0530

    Added Reschedule an Appointment modal

--- src/app/component/patient/appointments/appointment-details/appointment-details.component.ts
+++ src/app/component/patient/appointments/appointment-details/appointment-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { RescheduleAppointmentModalComponent } from 'src/app/shared/comman/reschedule-appointment-modal/reschedule-appointment-modal.component';
 
 @Component({
   selector: 'app-appointment-details', 
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './appointment-details.component.scss'
 })
 export class AppointmentDetailsComponent {
-
+  constructor(private router: Router, public dialog: MatDialog) { }
+  rescheduleModal(){
+    const dialogRef = this.dialog.open(RescheduleAppointmentModalComponent,{
+      panelClass: 'custom-alert-container',
+    });
+  }
 }
