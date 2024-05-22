a053c7bfaa4cc62c91f7abe7c097bb26a5a13b71
pankajkh <>
2024-05-22 21:41:43 +0530

    appointment listing ST

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -5,7 +5,8 @@ const User = require('../models/userModel');
 
 const getAppointmentList = async (req, res) => {
     try {
-        const { query, fields, order, offset, limit, patientFields, therapistFields, userQuery } = req.body;
+        const { query, queryTotal, fields, order, offset, limit, patientFields, therapistFields, userQuery } = req.body;
+
         if (userQuery && Object.keys(userQuery).length) {
             let userList = await User.find(userQuery, { _id: 1 });
             if (userList && userList.length > 0) {
@@ -18,7 +19,9 @@ const getAppointmentList = async (req, res) => {
             .populate('patientId', patientFields)
             .populate('therapistId', therapistFields)
             .sort(order).skip(offset).limit(limit)
-        let totalCount = await Appointment.find(query).countDocuments()
+
+        let totalCount = await Appointment.find(queryTotal).countDocuments()
+
         commonHelper.sendResponse(res, 'success', { appointmentList, totalCount }, '');
     } catch (error) {
         commonHelper.sendResponse(res, 'error', null, commonMessage.wentWrong);
