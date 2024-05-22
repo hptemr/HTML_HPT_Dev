c6f7fe29ceb2d16ff9e5239536150a3cf76469bf
thoratprashant <prashant.thorat@arkenea.com>
2024-05-22 17:54:41 +0530

    Done with Comman modals > UI

--- src/app/component/patient/appointments/appointment-details/appointment-details.component.ts
+++ src/app/component/patient/appointments/appointment-details/appointment-details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { RescheduleAppointmentModalComponent } from 'src/app/shared/comman/reschedule-appointment-modal/reschedule-appointment-modal.component';
+import { WriteCommentModalComponent } from 'src/app/shared/comman/write-comment-modal/write-comment-modal.component';
 
 @Component({
   selector: 'app-appointment-details', 
@@ -12,6 +13,11 @@ export class AppointmentDetailsComponent {
   constructor(private router: Router, public dialog: MatDialog) { }
   rescheduleModal(){
     const dialogRef = this.dialog.open(RescheduleAppointmentModalComponent,{
+      panelClass: 'custom-alert-container',
+    });
+  }
+  writeComment(){
+    const dialogRef = this.dialog.open(WriteCommentModalComponent,{
       panelClass: 'custom-alert-container',
     });
   }
