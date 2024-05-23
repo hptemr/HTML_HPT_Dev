c32d6dc71539323a357113d9de7e769350caec14
Manoj Nemade <manojn@arkenea.com>
2024-05-23 16:03:11 +0530

    appoinment models

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -13,9 +13,48 @@ const appointmentSchema = new mongoose.Schema({
     appointmentId: Number,
     appointmentDate: { type: Date, default: Date.now },
     practiceLocation: { type: String, default: "" },
-    checkIn:{ type: Boolean, default: false },
-    checkInDateTime:{ type: Date },
-    status: { type: String, enum: ['Pending', 'Approved', 'Cancelled', 'Completed',], default: 'Pending' },
+    checkIn: { type: Boolean, default: false },
+    checkInDateTime: { type: Date },
+    bookingFor: {
+        type: String,
+        enum: ['Myself', 'Other'],
+        default: 'Myself'
+    },
+    relationWithPatient: { type: String, default: "" },
+    patientInfo: {
+        type: Object //all the basic info will save in this object
+    },
+    payVia: {
+        type: String,
+        enum: ['Selfpay', 'Insurance'],
+        default: 'Selfpay'
+    },
+    
+    payViaInsuranceInfo: {
+        type: Object //all the info will save in this object, if patient select payVia insurance only.
+    },
+    responsiblePartyGuarantor: {
+        //If you are not the patient but responsible for charges then info will save in this column "responsiblePartyGuarantor"
+        type: Object //all the info will save in this object
+    },
+
+    emergencyContact1: { type: Object },
+    emergencyContact2: { type: Object },
+
+    reminderViaMobile: {
+        type: String,
+        enum: ['Yes', 'No'],
+        default: 'No'
+    },
+    reminderViaMobileNumber: { type: String, default: "" },
+
+    reminderViaEmail: {
+        type: String,
+        enum: ['Yes', 'No'],
+        default: 'No'
+    },
+    reminderViaEmailId: { type: String, default: "" },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 })
