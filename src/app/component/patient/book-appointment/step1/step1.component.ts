9a317ac847e47e154ed13f02520aa046a982da08
Manoj Nemade <manojn@arkenea.com>
2024-05-28 15:40:44 +0530

    book app step1

--- src/app/component/patient/book-appointment/step1/step1.component.ts
+++ src/app/component/patient/book-appointment/step1/step1.component.ts
@@ -3,24 +3,57 @@ import { MatRadioChange } from '@angular/material/radio';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { ContactModalComponent } from '../contact-modal/contact-modal.component';
 import { MatDialog } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { practiceLocations } from 'src/app/config';
 
 @Component({
-  selector: 'app-step1', 
+  selector: 'app-step1',
   templateUrl: './step1.component.html',
   styleUrl: './step1.component.scss'
 })
 export class Step1Component {
   model: NgbDateStruct;
   selectedValue: number;
-  onChange(event: MatRadioChange) {
-    console.log(this.selectedValue = event.value)
+
+  step1Form: FormGroup;
+  step1FormData: any
+
+  constructor(public dialog: MatDialog, private router: Router, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.step1FormData = localStorage.getItem("step1FormData")
+    this.loadForm()
   }
 
-  constructor(public dialog: MatDialog) { }
+  onChange(event: MatRadioChange) {
+    this.selectedValue = event.value
+  }
 
-  contactModal(){
-    const dialogRef = this.dialog.open(ContactModalComponent,{
-      panelClass: 'custom-alert-container', 
+
+  loadForm() {
+    this.step1Form = this.fb.group({
+      practiceLocation: [this.step1FormData ? this.step1FormData.practiceLocation : '', [Validators.required]],
+      appointmentDate: [this.step1FormData ? this.step1FormData.appointmentDate : '', [Validators.required]],
+      bookingFor: [this.step1FormData ? this.step1FormData.bookingFor : '', [Validators.required]],
+      relationWithPatient: [this.step1FormData ? this.step1FormData.relationWithPatient : '', [Validators.required]],
+      firstName: [this.step1FormData ? this.step1FormData.firstName : '', [Validators.required]],
+      middleName: [this.step1FormData ? this.step1FormData.middleName : ''],
+      lastName: [this.step1FormData ? this.step1FormData.lastName : '', [Validators.required]],
+      dob: [this.step1FormData ? this.step1FormData.dob : '', [Validators.required]],
+      martialStatus: [this.step1FormData ? this.step1FormData.martialStatus : '', [Validators.required]],
+      gender: [this.step1FormData ? this.step1FormData.gender : '', [Validators.required]],
+      email: [this.step1FormData ? this.step1FormData.email : '', [Validators.required]],
+      phoneNumber: [this.step1FormData ? this.step1FormData.phoneNumber : '', [Validators.required]],
+      cellPhoneNumber: [this.step1FormData ? this.step1FormData.cellPhoneNumber : ''],
+      workExtension: [this.step1FormData ? this.step1FormData.workExtension : ''],
+    });
+  }
+
+
+  contactModal() {
+    const dialogRef = this.dialog.open(ContactModalComponent, {
+      panelClass: 'custom-alert-container',
     });
   }
 }
--- src/app/component/patient/book-appointment/step2/step2.component.ts
+++ src/app/component/patient/book-appointment/step2/step2.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { CmsModalComponent } from 'src/app/shared/comman/cms-modal/cms-modal.component';
 import { AddInsuranceModalComponent } from '../add-insurance-modal/add-insurance-modal.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-step2', 
@@ -12,7 +14,40 @@ import { AddInsuranceModalComponent } from '../add-insurance-modal/add-insurance
 export class Step2Component {
   model: NgbDateStruct;
 
-  constructor(public dialog: MatDialog) { }
+  step2Form: FormGroup;
+  step2FormData: any
+
+  constructor(public dialog: MatDialog, private router: Router, private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.step2FormData = localStorage.getItem("step2FormData")
+    this.loadForm()
+  }
+
+
+  loadForm() {
+    this.step2Form = this.fb.group({
+      payVia: [this.step2FormData ? this.step2FormData.payVia : '', [Validators.required]],
+      relationWithPatient: [this.step2FormData ? this.step2FormData.relationWithPatient : '', [Validators.required]],
+      otherRelation: [this.step2FormData ? this.step2FormData.relationWithPatient : ''],
+     
+      firstName: [this.step2FormData ? this.step2FormData.firstName : '', [Validators.required]],
+      middleName: [this.step2FormData ? this.step2FormData.middleName : ''],
+      lastName: [this.step2FormData ? this.step2FormData.lastName : '', [Validators.required]],
+      dob: [this.step2FormData ? this.step2FormData.dob : '', [Validators.required]],
+      martialStatus: [this.step2FormData ? this.step2FormData.martialStatus : '', [Validators.required]],
+      gender: [this.step2FormData ? this.step2FormData.gender : '', [Validators.required]],
+      email: [this.step2FormData ? this.step2FormData.email : '', [Validators.required]],
+      phoneNumber: [this.step2FormData ? this.step2FormData.phoneNumber : '', [Validators.required]],
+      cellPhoneNumber: [this.step2FormData ? this.step2FormData.cellPhoneNumber : ''],
+      workExtension: [this.step2FormData ? this.step2FormData.workExtension : ''],
+
+
+
+      
+    });
+  }
+
 
   cmsModal(){
     const dialogRef = this.dialog.open(CmsModalComponent,{
