53664f1329c22a2de37ee7416289754a3c659944
Manoj Nemade <manojn@arkenea.com>
2024-06-05 16:51:11 +0530

    step4- done and step3 wip

--- controllers/emergencyContactController.js
+++ controllers/emergencyContactController.js
@@ -6,52 +6,49 @@ const addUpdateEmergencyContact = async (req, res) => {
     try {
         const { query, data } = req.body;
         let result = {};
-        if(data.contacts){
+        if (data.contacts) {
             await EmergencyContact.deleteMany({ patientId: query._id });
-            for(i=0;i<data.contacts.length;i++){
-                let item = data.contacts[i];        
-                let dob = ''; 
-                if(item.dob){
+            for (i = 0; i < data.contacts.length; i++) {
+                let item = data.contacts[i];
+                let dob = '';
+                if (item.dob) {
                     dob = item.dob;
-                    dob = dob.year+'-'+dob.month+'-'+dob.day;
+                    dob = dob.year + '-' + dob.month + '-' + dob.day;
                 }
-                 let request_data = {
-                        patientId:query._id,
-                        firstName:item.firstName,
-                        lastName:item.lastName,
-                        dob:dob,
-                        relationWithPatient:item.relationWithPatient,
-                        otherRelation:item.otherRelation,
-                        phoneNumber:item.phoneNumber,
-                        myTreatmentCheckbox:item.myTreatmentCheckbox,
-                        myAccountCheckbox:item.myAccountCheckbox,
-                        orderIndex:i+1
-                    }    
+                let request_data = {
+                    patientId: query._id,
+                    firstName: item.firstName,
+                    lastName: item.lastName,
+                    dob: dob,
+                    relationWithPatient: item.relationWithPatient,
+                    otherRelation: item.otherRelation,
+                    phoneNumber: item.phoneNumber,
+                    myTreatmentCheckbox: item.myTreatmentCheckbox,
+                    myAccountCheckbox: item.myAccountCheckbox,
+                    orderIndex: i + 1
+                }
                 let newContact = new EmergencyContact(request_data);
-                result = await newContact.save();                
+                result = await newContact.save();
             }
-        }        
-        commonHelper.sendResponse(res, 'success', null, emergencyContactsMessage.saved);     
+        }
+        commonHelper.sendResponse(res, 'success', null, emergencyContactsMessage.saved);
     } catch (error) {
-        console.log('error>>>',error);
+        console.log('error>>>', error);
         commonHelper.sendResponse(res, 'error', null, commonMessage.wentWrong);
     }
 }
 
 const getContactData = async (req, res) => {
     try {
-        const { query } = req.body;
-
-        let records = await EmergencyContact.find({patientId:query._id});
-
+        const { query, order } = req.body;
+        let records = await EmergencyContact.find({ patientId: query._id }).sort(order);
         let emergencyContactList = [];
-        if(records && records.length>0){
+        if (records && records.length > 0) {
             emergencyContactList = records;
         }
-
-        commonHelper.sendResponse(res, 'success', emergencyContactList, '');     
+        commonHelper.sendResponse(res, 'success', emergencyContactList, '');
     } catch (error) {
-        console.log('error>>>',error);
+        console.log('error>>>', error);
         commonHelper.sendResponse(res, 'error', null, commonMessage.wentWrong);
     }
 }
