2875535de0fa63222c4231fce1d84358c14684e4
satyajitarkenea24 <satyajit@arkenea.com>
2024-06-18 21:24:45 +0530

    select-folder and files ui done

--- src/app/component/therapist/protocol/select-files/select-files.component.ts
+++ src/app/component/therapist/protocol/select-files/select-files.component.ts
@@ -1,4 +1,78 @@
-import { Component } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  actions: string;
+  icon: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  { 
+    name: '19G NOTE VERBIAGE',  
+    actions: '' ,
+    icon: 'folder'  
+  }, 
+  { 
+    name: 'ADDING PROVIDER STEPS',  
+    actions: '',
+    icon: 'description'    
+  }, 
+  { 
+    name: 'CST FAXES',  
+    icon: 'picture_as_pdf',   
+    actions: ''   
+  }, 
+  { 
+    name: 'EXT LIST',
+    icon: 'folder',  
+    actions: ''   
+  },   
+  { 
+    name: 'MEDICAID PPA Instuctions',
+    icon: 'picture_as_pdf',  
+    actions: ''   
+  },
+  { 
+    name: 'NRO POC Track Sheet',
+    icon: 'description',  
+    actions: ''   
+  },
+  { 
+    name: '19G NOTE VERBIAGE',  
+    actions: '' ,
+    icon: 'description'  
+  }, 
+  { 
+    name: 'ADDING PROVIDER STEPS',  
+    actions: '',
+    icon: 'description'    
+  }, 
+  { 
+    name: 'CST FAXES',  
+    icon: 'picture_as_pdf',   
+    actions: ''   
+  }, 
+  { 
+    name: 'EXT LIST',
+    icon: 'description',  
+    actions: ''   
+  },   
+  { 
+    name: 'MEDICAID PPA Instuctions',
+    icon: 'picture_as_pdf',  
+    actions: ''   
+  },
+  { 
+    name: 'NRO POC Track Sheet',
+    icon: 'description',  
+    actions: ''   
+  },
+   
+];
 
 @Component({
   selector: 'app-select-files', 
@@ -6,5 +80,25 @@ import { Component } from '@angular/core';
   styleUrl: './select-files.component.scss'
 })
 export class SelectFilesComponent {
+  displayedColumns: string[] = ['name', 'actions'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  constructor(private _liveAnnouncer: LiveAnnouncer, public dialog: MatDialog) { }
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+    /** Announce the change in sort state for assistive technology. */
+    announceSortChange(sortState: Sort) { 
+      if (sortState.direction) {
+        this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+      } else {
+        this._liveAnnouncer.announce('Sorting cleared');
+      }
+    }
 }
--- src/app/component/therapist/protocol/select-folder/select-folder.component.ts
+++ src/app/component/therapist/protocol/select-folder/select-folder.component.ts
@@ -1,4 +1,60 @@
-import { Component } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;   
+  actions: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  { 
+    name: 'ALLMACHER',  
+    actions: ''   
+  }, 
+  { 
+    name: 'AMRINE',  
+    actions: ''   
+  }, 
+  { 
+    name: 'APRIL WEINBERGER AND Y. COURCHESNE',  
+    actions: ''   
+  }, 
+  { 
+    name: 'BECK',  
+    actions: ''   
+  },
+  {
+    name: 'BREWINGTON',
+    actions: ''
+  },
+  {
+    name: 'BUCKLEY',
+    actions: ''
+  },
+  {
+    name: 'CHANNER',
+    actions: ''
+  },
+  {
+    name: 'DOLECKI',
+    actions: ''
+  },   
+  {
+    name: 'JACOBSON',
+    actions: ''
+  },
+  {
+    name: 'M. WOODS',
+    actions: ''
+  },
+  {
+    name: 'Valerie Chvle. ARNP',
+    actions: ''
+  },
+];
 
 @Component({
   selector: 'app-select-folder', 
@@ -6,5 +62,24 @@ import { Component } from '@angular/core';
   styleUrl: './select-folder.component.scss'
 })
 export class SelectFolderComponent {
+  displayedColumns: string[] = ['name', 'actions'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  constructor(private _liveAnnouncer: LiveAnnouncer, public dialog: MatDialog) {}
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+    /** Announce the change in sort state for assistive technology. */
+    announceSortChange(sortState: Sort) {
+      if (sortState.direction) {
+        this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+      } else {
+        this._liveAnnouncer.announce('Sorting cleared');
+      }
+    }
 }
