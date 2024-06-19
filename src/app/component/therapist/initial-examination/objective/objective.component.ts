a9cbcf8c2ffd514da3ca443e613965e1eaf5dec5
thoratprashant <prashant.thorat@arkenea.com>
2024-06-19 22:09:38 +0530

    UI

--- src/app/component/therapist/initial-examination/objective/objective.component.ts
+++ src/app/component/therapist/initial-examination/objective/objective.component.ts
@@ -11,6 +11,33 @@ export class ObjectiveComponent {
   selectedValue = '0';
   
   clickedIndex = 0; 
+  clickedIndex2 = 0;
+  clickedIndex3 = 0;
+  clickedIndex4 = 0;
+  clickedIndex5 = 0;
+  clickedIndex6 = 0;
+  clickedIndex7 = 0;
+  clickedIndex8 = 0;
+  clickedIndex9 = 0;
+  clickedIndex10 = 0; 
+  clickedIndex11 =0;
+  clickedIndex12 = 0;
+  clickedIndex13 = 0;
+  clickedIndex14 = 0;
+  clickedIndex15 = 0;
+  clickedIndex16 = 0;
+  clickedIndex17 = 0;
+  clickedIndex18 = 0;
+  clickedIndex19 = 0; 
+  clickedIndex20 = 0;
+  clickedIndex21 = 0;
+  clickedIndex22 = 0;
+  clickedIndex23 = 0;
+  clickedIndex24 = 0;
+  clickedIndex25 = 0;
+  clickedIndex26 = 0;
+  clickedIndex27 = 0;
+  clickedIndex28 = 0;
   tabs = [
     {number: '1'}, {number: '2'}, {number: '3'},
     {number: '4'}, {number: '5'}, {number: '6'},
