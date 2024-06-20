b45da2a7f191e355c6af1a3239c47646efa93feb
Manoj Nemade <manojn@arkenea.com>
2024-06-20 10:37:11 +0530

    intake form links added to support team

--- src/app/component/admin/support-team/support-team-routing.module.ts
+++ src/app/component/admin/support-team/support-team-routing.module.ts
@@ -13,6 +13,11 @@ import { AppointmentDetailsComponent } from 'src/app/shared/component/support-bi
 import { AppointmentRequestsComponent } from 'src/app/shared/component/support-billing-therapist/appointment-requests/appointment-requests.component';
 //import { SystemFollowupModalComponent } from 'src/app/shared/component/support-billing-therapist/system-followup-modal/system-followup-modal.component';
 import { ManageProfileComponent } from 'src/app/shared/component/manage-profile/manage-profile.component';
+import { IntakeStep1Component } from 'src/app/shared/component/intake-form/intake-step1/intake-step1.component';
+import { IntakeStep2Component } from 'src/app/shared/component/intake-form/intake-step2/intake-step2.component';
+import { IntakeStep3Component } from 'src/app/shared/component/intake-form/intake-step3/intake-step3.component';
+import { IntakeStep4Component } from 'src/app/shared/component/intake-form/intake-step4/intake-step4.component';
+import { IntakeStep5Component } from 'src/app/shared/component/intake-form/intake-step5/intake-step5.component';
 
 const routes: Routes = [
   {
@@ -39,6 +44,32 @@ const routes: Routes = [
   //   path: 'appointment-requests',
   //   component: AppointmentRequestsComponent
   // },
+
+  {
+    path: 'intake-form',
+    children: [
+      {
+        path: 'step-1/:appId',
+        component: IntakeStep1Component,
+      },
+      {
+        path: 'step-2/:appId',
+        component: IntakeStep2Component,
+      },
+      {
+        path: 'step-3/:appId',
+        component: IntakeStep3Component,
+      },
+      {
+        path: 'step-4/:appId',
+        component: IntakeStep4Component,
+      },
+      {
+        path: 'step-5/:appId',
+        component: IntakeStep5Component,
+      },
+    ]
+  },
 ];
 
 @NgModule({
