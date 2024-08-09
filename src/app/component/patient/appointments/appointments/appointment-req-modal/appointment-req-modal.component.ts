1950596ea5461622c03720da339d20f9cabc80a4
pankajkh <>
2024-08-09 20:37:52 +0530

    app request

--- src/app/component/patient/appointments/appointments/appointment-req-modal/appointment-req-modal.component.ts
+++ src/app/component/patient/appointments/appointments/appointment-req-modal/appointment-req-modal.component.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { SuccessModalComponent } from 'src/app/shared/comman/success-modal/success-modal.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatDialog } from '@angular/material/dialog';
-
+import { practiceLocations, s3Details } from 'src/app/config';
+import { AuthService } from 'src/app/shared/services/api/auth.service';
+import { CommonService } from 'src/app/shared/services/helper/common.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { validationMessages } from '../../../../../utils/validation-messages';
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-reschedule-appointment-modal',
   standalone: true,
@@ -21,45 +27,62 @@ export class AppointmentReqModalComponent {
   fromDate: any
   toDate: any;
   whereCond: any = {}
-  minToDate: any
+  minToDate: any = new Date()
   maxFromDate: any
-  constructor(public dialog: MatDialog) {
+  requestAppointmentForm: FormGroup;
+  validationMessages = validationMessages
+  practiceLocationData: string[] = practiceLocations
+  public userId: string = this.authService.getLoggedInInfo('_id');
+  public userRole: string = this.authService.getLoggedInInfo('role');
+  clickOnRequestAppointment:boolean=false;
+  constructor(public dialog: MatDialog,
+    public authService: AuthService,
+    public commonService: CommonService,
+    private fb: FormBuilder,
+    private router: Router,
+    public dialogRef: MatDialogRef<AppointmentReqModalComponent>,
+  ) {
   }
  
   ngOnInit() {
-    this.getAppointmentList()
+    console.log('userId>>>',this.userId)
+    this.requestAppointmentForm = this.fb.group({
+      appointmentDate: ['', [Validators.required]],
+      practiceLocation: ['',[Validators.required]],
+    });
+    //this.getAppointmentList()
   }
-  getAppointmentList() {
-    throw new Error('Method not implemented.');
-  }
-
-  onDateChange(event: any, colName: any) {
-    if (colName == 'fromDate') {
-      this.minToDate = new Date(event.target.value)
+ 
+  async requestAppointment(formData:any){    
+    if (this.requestAppointmentForm.valid) {
+        this.clickOnRequestAppointment = true
+        this.commonService.showLoader();
+        Object.assign(formData, {patientId: this.userId})
+        let reqVars = {
+          userId: this.userId,
+          data: formData
+        }
+        this.commonService.showLoader();       
+        await this.authService.apiRequest('post', 'appointment/createAppointmentRequest',reqVars).subscribe(async response => { 
+          this.commonService.hideLoader();
+          this.dialogRef.close();
+          if (response.error) {
+            if(response.message){
+              this.commonService.openSnackBar(response.message, "ERROR")   
+            }
+          } else {          
+            if(response.message){
+              this.commonService.openSnackBar(response.message, "SUCCESS")   
+            }          
+          //this.router.navigate(['/support-team/referrals']);
+          this.successModal();
+          }
+        })
+    }else{
+        this.requestAppointmentForm.markAllAsTouched();
+        return;  
     }
-    let dateCond
-    if (this.fromDate && this.toDate) {
-      dateCond = {
-        appointmentDate: {
-          $gte: this.fromDate,
-          $lte: this.toDate
-        }
-      }
-    } else {
-      if (this.fromDate) {
-        dateCond = {
-          appointmentDate: { $gte: this.fromDate }
-        }
-      } else {
-        dateCond = {
-          appointmentDate: { $lte: this.toDate }
-        }
-      }
-    }
-    Object.assign(this.whereCond, dateCond)
-    this.getAppointmentList()
   }
-
 
   
   successModal() {
@@ -71,5 +94,36 @@ export class AppointmentReqModalComponent {
       }
     });
   }
+
+   // getAppointmentList() {
+  //   throw new Error('Method not implemented.');
+  // }
+  // onDateChange(event: any, colName: any) {
+  //   if (colName == 'fromDate') {
+  //     this.minToDate = new Date(event.target.value)
+  //   }
+  //   let dateCond
+  //   if (this.fromDate && this.toDate) {
+  //     dateCond = {
+  //       appointmentDate: {
+  //         $gte: this.fromDate,
+  //         $lte: this.toDate
+  //       }
+  //     }
+  //   } else {
+  //     if (this.fromDate) {
+  //       dateCond = {
+  //         appointmentDate: { $gte: this.fromDate }
+  //       }
+  //     } else {
+  //       dateCond = {
+  //         appointmentDate: { $lte: this.toDate }
+  //       }
+  //     }
+  //   }
+  //   Object.assign(this.whereCond, dateCond)
+  //   //this.getAppointmentList()
+  // }
+
   
 }
