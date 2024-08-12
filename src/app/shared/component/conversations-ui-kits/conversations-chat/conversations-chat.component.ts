95a99007798b4e6b4e78a63e39383172d8323091
thoratprashant <prashant.thorat@arkenea.com>
2024-08-12 17:48:42 +0530

    comet-chat UI

--- src/app/shared/component/conversations-ui-kits/conversations-chat/conversations-chat.component.ts
+++ src/app/shared/component/conversations-ui-kits/conversations-chat/conversations-chat.component.ts
@@ -5,6 +5,7 @@ import { Location } from '@angular/common';
 import { CreateChatGroupComponent } from '../create-chat-group/create-chat-group.component';
 import { MatDialog } from '@angular/material/dialog';
 import { CommonService } from 'src/app/shared/services/helper/common.service';
+import { BaseStyle } from '@cometchat/uikit-elements';
 @Component({
   selector: 'app-conversations-chat',
   templateUrl: './conversations-chat.component.html',
@@ -14,12 +15,17 @@ export class ConversationsChatComponent {
   withMessagesStyle:any={
     width: "100%",
     height: "100%",
-    background: "transparent",
+    // background: "transparent",
+     background: "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)",
     borderRadius: "none",
     border: "none",
     messageTextColor: "rgba(20, 20, 20, 0.33)", 
     textFont: "Nunito Sans, sans-serif"
   }
+
+  bStyle = new BaseStyle({
+    background: "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)"
+  });
 
   public replaceTheme:boolean = false;
   isMobileView: boolean=false;
