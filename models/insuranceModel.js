9cc61abb4c2e650dcb6ed61d80cba294bb8e57ac
pankajkh <>
2024-08-30 21:07:04 +0530

    third inssurance

--- models/insuranceModel.js
+++ models/insuranceModel.js
@@ -16,11 +16,22 @@ const insurnaceSchema = new mongoose.Schema({
     primaryInsuranceIdPolicy: { type: String, default: "" },
     primaryInsuranceGroup: { type: String, default: "" },
     primaryInsuranceCustomerServicePh: { type: String, default: "" },
+    primaryInsuranceFromDate: { type: Date, default: "" },
+    primaryInsuranceToDate: { type: Date, default: "" },
 
     secondaryInsuranceCompany: { type: String, default: "" },
     secondaryInsuranceIdPolicy: { type: String, default: "" },
     secondaryInsuranceGroup: { type: String, default: "" },
     secondaryInsuranceCustomerServicePh: { type: String, default: "" },
+    secondaryInsuranceFromDate: { type: Date, default: "" },
+    secondaryInsuranceToDate: { type: Date, default: "" },
+
+    thirdInsuranceCompany: { type: String, default: "" },
+    thirdInsuranceIdPolicy: { type: String, default: "" },
+    thirdInsuranceGroup: { type: String, default: "" },
+    thirdInsuranceCustomerServicePh: { type: String, default: "" },
+    thirdInsuranceFromDate: { type: Date, default: "" },
+    thirdInsuranceToDate: { type: Date, default: "" },
 
     injuryRelelatedTo: { type: String, default: "" },
     carrierName: { type: String, default: "" },
