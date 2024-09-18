2b4140febc00b38f7191f941b7666f3aa596af2b
thoratprashant <prashant.thorat@arkenea.com>
2024-09-18 11:17:52 +0530

    UI

--- src/app/component/billing-team/billing-details/billing-details.component.ts
+++ src/app/component/billing-team/billing-details/billing-details.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { SelectPrimaryInsuModalComponent } from 'src/app/shared/comman/select-primary-insu-modal/select-primary-insu-modal.component';
 
 @Component({
   selector: 'app-billing-details',
@@ -7,5 +9,14 @@ import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './billing-details.component.scss'
 })
 export class BillingDetailsComponent {
+  
   model: NgbDateStruct;
+  constructor(public dialog: MatDialog, ) {}
+
+  selectInsuranceModal() {
+    const dialogRef = this.dialog.open(SelectPrimaryInsuModalComponent,{
+      width:'650px',
+      panelClass: [ 'modal--wrapper'],
+    });
+  }
 }
