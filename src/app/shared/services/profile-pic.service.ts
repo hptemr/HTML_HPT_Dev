6ffba54dbf7f7954388fac19fc4419dbaa8eb9d9
pankajkh <>
2024-09-27 21:30:03 +0530

    Jira issus

--- src/app/shared/services/profile-pic.service.ts
+++ src/app/shared/services/profile-pic.service.ts
@@ -1,9 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { AuthService } from './api/auth.service';
 import { s3Details } from 'src/app/config';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -13,19 +11,11 @@ export class ProfilePicService {
 //  private triggerSubject = new Subject<void>();
 //  triggerObservable$ = this.triggerSubject.asObservable();
 
- constructor(private authService: AuthService) {}  
+ constructor() {}  
 
  set setProfilePic(value:any) {
-  console.log('hi here also',value)
-  console.log('hi here also...........',s3Details.awsS3Url + s3Details.userProfileFolderPath+value)
    this.itemValue.next(s3Details.awsS3Url + s3Details.userProfileFolderPath+value);
  }
-
-
-//  get getProfilePic() {
-//   this.profileImage = s3Details.awsS3Url + s3Details.userProfileFolderPath + this.authService.getLoggedInInfo('profileImage')
-//    return this.profileImage;
-//  }
 
 //  triggerHeaderUpdate(profileImage:any) {
 //   console.log('hi here.........',profileImage)
