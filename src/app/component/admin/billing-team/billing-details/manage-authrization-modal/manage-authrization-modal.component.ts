6968bfba0d862a5abb2cff3ac80874eb2ee3020d
Ashish <shahnawaz@arkenea.com>
2024-10-05 23:58:22 +0530

    billing details add and updates

--- src/app/component/admin/billing-team/billing-details/manage-authrization-modal/manage-authrization-modal.component.ts
+++ src/app/component/admin/billing-team/billing-details/manage-authrization-modal/manage-authrization-modal.component.ts
@@ -1,21 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule, MatDialog, MatDialogRef, MAT_DIALOG_DATA  } from '@angular/material/dialog';
+import { CommonService } from 'src/app/shared/services/helper/common.service';
+import { AuthService } from 'src/app/shared/services/api/auth.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { validationMessages } from '../../../../../utils/validation-messages'
+import { CommonModule } from '@angular/common'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { regex } from '../../../../../utils/regex-patterns';
 
 @Component({
   selector: 'app-manage-authrization-modal',
   standalone: true,
-  imports: [MatInputModule ,MatIconModule, MatButtonModule, MatDialogModule, MatTableModule, MatCheckboxModule, MatRadioModule, MatDatepickerModule],
-
+  imports: [MatInputModule ,MatIconModule, MatButtonModule, MatDialogModule, MatTableModule, MatCheckboxModule, MatRadioModule, MatDatepickerModule, CommonModule, FormsModule,ReactiveFormsModule],
   templateUrl: './manage-authrization-modal.component.html',
   styleUrl: './manage-authrization-modal.component.scss'
 })
 export class ManageAuthrizationModalComponent {
+  validationMessages = validationMessages
+  patientId:any;
+  caseName:any;
+  authorizationManagement: FormGroup;
+  authorizationFromDate:any=''
+
+  constructor( 
+    private commonService: CommonService,
+    private authService: AuthService,
+    private fb: FormBuilder, 
+    public dialog: MatDialog,
+    private dialogRef: MatDialogRef<ManageAuthrizationModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { 
+  }
+
+  ngOnInit() {
+    this.patientId = this.data.patientId
+    this.caseName = this.data.caseName
+    this.initializeAuthorizationManagementForm()
+  }
+
+  initializeAuthorizationManagementForm(){
+    this.authorizationManagement = this.fb.group({
+      authorizationRequired: ['', [Validators.required]],
+      authorizationToDate: ['', [Validators.required]],
+      authorizationFromDate: ['', [Validators.required]],
+      authorizationVisit: ['', [Validators.required,Validators.pattern(regex.onlyNumeric)]],
+      authorizationNumber: ['', [Validators.required,Validators.pattern(regex.onlyNumeric)]]
+    })
+  }
+
+  onAuthorizationFromDateChange(event: any) {
+    let selectedDate = new Date(event.value);
+    this.authorizationFromDate = selectedDate;
+  }
+
+
+  saveAuthorizationManagement(){
+    if(this.authorizationManagement.valid){
+      this.commonService.showLoader();
+      let authManagementObj:any = {
+        authorizationManagementData : this.authorizationManagement.value,
+        patientId : this.patientId,
+        caseName : this.caseName
+      }
+
+      // this.authService.apiRequest('post', 'appointment/addAuthorizationManagement', authManagementObj).subscribe(async response => {  
+      //   this.commonService.openSnackBar(response.message, "SUCCESS")
+      //   this.commonService.hideLoader(); 
+      // },(err) => {
+      //   this.commonService.hideLoader();
+      //   err.error?.error ? this.commonService.openSnackBar(err.error?.message, "ERROR") : ''
+      // })
+    }
+  }
 
 }
