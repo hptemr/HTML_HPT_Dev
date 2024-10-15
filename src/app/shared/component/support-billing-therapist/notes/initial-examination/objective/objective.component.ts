c623c034d499c65cc348e1fbe0c9ae0534a80a95
guru@1150 <guru@arkenea.com>
2024-10-15 16:35:07 +0530

    UI

--- src/app/shared/component/support-billing-therapist/notes/initial-examination/objective/objective.component.ts
+++ src/app/shared/component/support-billing-therapist/notes/initial-examination/objective/objective.component.ts
@@ -100,34 +100,6 @@ export class ObjectiveComponent {
       weight:'50 lbs',
       distance: '1',
       time: 'NA'
-    },
-    {
-      set:'2',
-      reps:'20',
-      weight:'20 kg',
-      distance: '1.5',
-      time: '5 min'
-    },
-    {
-      set:'3',
-      reps:'30',
-      weight:'100 kg',
-      distance: '2',
-      time: '10 min'
-    },
-    {
-      set:'4',
-      reps:'40',
-      weight:'80 lbs',
-      distance: '2.5',
-      time: '300 sec'
-    },
-    {
-      set:'5',
-      reps:'50',
-      weight:'40 lbs',
-      distance: '3',
-      time: '800 sec'
-    },
+    }, 
   ]
 }
