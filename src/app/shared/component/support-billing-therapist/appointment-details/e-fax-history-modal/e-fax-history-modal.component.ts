636c5feb69d15a6cae4f885428dcc5eeb8192a87
guru <guru@arkenea.com>
2024-11-11 16:06:11 +0530

    fax history

--- src/app/shared/component/support-billing-therapist/appointment-details/e-fax-history-modal/e-fax-history-modal.component.ts
+++ src/app/shared/component/support-billing-therapist/appointment-details/e-fax-history-modal/e-fax-history-modal.component.ts
@@ -1,57 +1,31 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/api/auth.service';
 
+
+export interface PeriodicElement {
+  noteType: string;
+  dateOfService: string;
+  createdAt: string;
+  status: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [];
 @Component({
   selector: 'app-e-fax-history-modal', 
   templateUrl: './e-fax-history-modal.component.html',
   styleUrl: './e-fax-history-modal.component.scss'
 })
 export class EFaxHistoryModalComponent {
-  displayedColumns: string[] = ['dateofservice', 'noteType', 'sentOn', 'status'];
+  displayedColumns: string[] = ['dateOfService', 'noteType', 'createdAt', 'status'];
   dataSource = ELEMENT_DATA;
+
+  constructor(public authService: AuthService){
+    let reqVars = {}
+    this.authService.apiRequest('post', 'soapNote/getFaxHistory', reqVars).subscribe(response => {
+      this.dataSource = response.data
+    })
+  }
+
 }
 
-export interface PeriodicElement {
-  noteType: string;
-  dateofservice: string;
-  sentOn: string;
-  status: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    dateofservice: '08/10/2024', 
-    noteType: 'Daily Note', 
-    sentOn: '08/08/2024', 
-    status: 'Success'
-  }, 
-  {
-    dateofservice: '10/10/2024', 
-    noteType: 'Progress Note', 
-    sentOn: '17/10/2024', 
-    status: 'Success'
-  },
-  {
-    dateofservice: '08/10/2024', 
-    noteType: 'Daily Note', 
-    sentOn: '08/08/2024', 
-    status: 'Success'
-  }, 
-  {
-    dateofservice: '10/10/2024', 
-    noteType: 'Progress Note', 
-    sentOn: '17/10/2024', 
-    status: 'Failure'
-  },
-  {
-    dateofservice: '08/10/2024', 
-    noteType: 'Daily Note', 
-    sentOn: '08/08/2024', 
-    status: 'Success'
-  }, 
-  {
-    dateofservice: '10/10/2024', 
-    noteType: 'Progress Note', 
-    sentOn: '17/10/2024', 
-    status: 'Failure'
-  },
-];
+
