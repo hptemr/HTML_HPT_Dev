96648e0fd2feef63e05cbbf64448283fc272a220
guru <guru@arkenea.com>
2024-12-03 16:00:58 +0530

    added charts

--- src/app/shared/comman/reports/reports.component.ts
+++ src/app/shared/comman/reports/reports.component.ts
@@ -9,33 +9,33 @@ import { CommonService } from '../../services/helper/common.service';
 import { AuthService } from '../../services/api/auth.service';
 import { practiceLocations } from 'src/app/config';
 
-// import {
-//   ChartComponent,
-//   ApexAxisChartSeries,
-//   ApexChart,
-//   ApexXAxis,
-//   ApexDataLabels,
-//   ApexStroke,
-//   ApexMarkers,
-//   ApexYAxis,
-//   ApexGrid,
-//   ApexTitleSubtitle,
-//   ApexLegend
-// } from "ng-apexcharts";
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexStroke,
+  ApexMarkers,
+  ApexYAxis,
+  ApexGrid,
+  ApexTitleSubtitle,
+  ApexLegend
+} from "ng-apexcharts";
 
-// export type ChartOptions = {
-//   series: ApexAxisChartSeries;
-//   chart: ApexChart;
-//   xaxis: ApexXAxis;
-//   stroke: ApexStroke;
-//   dataLabels: ApexDataLabels;
-//   markers: ApexMarkers;
-//   tooltip: any; // ApexTooltip;
-//   yaxis: ApexYAxis;
-//   grid: ApexGrid;
-//   legend: ApexLegend;
-//   title: ApexTitleSubtitle;
-// };
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+  dataLabels: ApexDataLabels;
+  markers: ApexMarkers;
+  tooltip: any; // ApexTooltip;
+  yaxis: ApexYAxis;
+  grid: ApexGrid;
+  legend: ApexLegend;
+  title: ApexTitleSubtitle;
+};
 
 
 
@@ -1225,8 +1225,8 @@ export class ReportsComponent {
   dataSource10 = new MatTableDataSource(ELEMENT_DATA_10);
 
 
-  // @ViewChild("chart") chart: ChartComponent;
-  // public chartOptions: Partial<ChartOptions>;
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
   practiceLocations = practiceLocations
 
@@ -1235,109 +1235,109 @@ export class ReportsComponent {
     private authService: AuthService,
 
   ) {
-    // this.chartOptions = { 
-    //   series: [
-    //     {
-    //       name: "2023",
-    //       data: [45, 52, 38, 24, 33, 26, 21, 20, 60, 18, 40, 100],
-    //     },
-    //     {
-    //       name: "2024",
-    //       data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35],
-    //     }, 
-    //   ], 
-    //   chart: {
-    //     height: 400,
-    //     type: "line",
-    //      fontFamily: 'Nunito Sans', 
-    //      toolbar: {
-    //       show: false
-    //     },
-    //     zoom: {
-    //       enabled: false,
-    //     }
-    //   },
+    this.chartOptions = { 
+      series: [
+        {
+          name: "2023",
+          data: [45, 52, 38, 24, 33, 26, 21, 20, 60, 18, 40, 100],
+        },
+        {
+          name: "2024",
+          data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35],
+        }, 
+      ], 
+      chart: {
+        height: 400,
+        type: "line",
+         fontFamily: 'Nunito Sans', 
+         toolbar: {
+          show: false
+        },
+        zoom: {
+          enabled: false,
+        }
+      },
 
-    //   dataLabels: {
-    //     enabled: false
-    //   },
-    //   stroke: {
-    //     width: 2,
-    //     curve: "straight",
-    //     dashArray: [0, 8, 5]
-    //   },
-    //   title: {
-    //     text: "Total Patients Seen Comparison",
-    //     style: { 
-    //       fontWeight: "700",
-    //       fontSize: "18px",
-    //     }
-    //   },
-    //   legend: {
-    //     tooltipHoverFormatter: function(val, opts) {
-    //       return (
-    //         val +
-    //         " - <strong>" +
-    //         opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
-    //         "</strong>"
-    //       );
-    //     }
-    //   },
-    //   markers: {
-    //     size: 0,
-    //     hover: {
-    //       sizeOffset: 6
-    //     },
-    //     colors: ['#000', '#f1f1f1',  ]
-    //   },
-    //   xaxis: {
-    //     labels: {
-    //       trim: false
-    //     },
-    //     categories: [
-    //       "Jan",
-    //       "Feb",
-    //       "Mar",
-    //       "Apr",
-    //       "May",
-    //       "Jun",
-    //       "Jul",
-    //       "Aug",
-    //       "Sep",
-    //       "Oct",
-    //       "Nov",
-    //       "Dec"
-    //     ]
-    //   },
-    //   tooltip: {
-    //     y: [
-    //       {
-    //         title: {
-    //           formatter: function(val: string) {
-    //             return val + " (mins)";
-    //           }
-    //         }
-    //       },
-    //       {
-    //         title: {
-    //           formatter: function(val: string) {
-    //             return val + " per session";
-    //           }
-    //         }
-    //       },
-    //       {
-    //         title: {
-    //           formatter: function(val: any) {
-    //             return val;
-    //           }
-    //         }
-    //       }
-    //     ]
-    //   },
-    //   grid: {
-    //     borderColor: "#f1f1f1"
-    //   }
-    // };
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        width: 2,
+        curve: "straight",
+        dashArray: [0, 8, 5]
+      },
+      title: {
+        text: "Total Patients Seen Comparison",
+        style: { 
+          fontWeight: "700",
+          fontSize: "18px",
+        }
+      },
+      legend: {
+        tooltipHoverFormatter: function(val, opts) {
+          return (
+            val +
+            " - <strong>" +
+            opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
+            "</strong>"
+          );
+        }
+      },
+      markers: {
+        size: 0,
+        hover: {
+          sizeOffset: 6
+        },
+        colors: ['#000', '#f1f1f1',  ]
+      },
+      xaxis: {
+        labels: {
+          trim: false
+        },
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec"
+        ]
+      },
+      tooltip: {
+        y: [
+          {
+            title: {
+              formatter: function(val: string) {
+                return val + " (mins)";
+              }
+            }
+          },
+          {
+            title: {
+              formatter: function(val: string) {
+                return val + " per session";
+              }
+            }
+          },
+          {
+            title: {
+              formatter: function(val: any) {
+                return val;
+              }
+            }
+          }
+        ]
+      },
+      grid: {
+        borderColor: "#f1f1f1"
+      }
+    };
   }
 
   ngOnInit() {
